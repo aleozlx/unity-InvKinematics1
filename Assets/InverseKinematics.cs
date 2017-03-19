2ff6619f3e292dad99ae0e4c399d438422c64ba1
Alex Yang <aleozlx@gmail.com>
2017-03-19 15:03:51 -0500

    init

---
+++ Assets/InverseKinematics.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InverseKinematics : MonoBehaviour {
+	public GameObject ball;
+	public HingeJoint platformHindge;
+
+	void Start () {
+		ball = GameObject.Find("Ball");
+		platformHindge = GetComponent<HingeJoint>();
+	}
+
+	void Update () {
+		JointSpring hingeSpring = platformHindge.spring;
+		hingeSpring.targetPosition = ball.transform.localPosition.y;
+		platformHindge.spring = hingeSpring;
+	}
+}
