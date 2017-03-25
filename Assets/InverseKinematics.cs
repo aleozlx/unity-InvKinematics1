5da9b976c93c0fc8de6e92acde12fd2190ba7859
Alex Yang <aleozlx@gmail.com>
2017-03-25 01:04:09 -0500

    invkinematics

--- Assets/InverseKinematics.cs
+++ Assets/InverseKinematics.cs
@@ -3,31 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InverseKinematics : MonoBehaviour {
-	public GameObject ball, test;
-	public GameObject arm1, arm2;
-	public HingeJoint joint1, joint2;
+	public HingeJoint[] joints;
+	public GameObject target;
 
 	void Start () {
-		ball = GameObject.Find ("Ball");
-		arm1 = GameObject.Find ("Arm1");
-		arm2 = GameObject.Find ("Arm2");
-		test = GameObject.Find ("Test");
-		joint1 = GetComponent<HingeJoint>();
-		joint2 = arm1.GetComponent<HingeJoint> ();
+		// target = GameObject.Find ("Ball");
+	}
+
+	static Vector3 Follow(HingeJoint joint, Vector3 target){
+		const float rate = 0.1f;
+		var anchor = joint.gameObject.transform.TransformPoint (joint.anchor);
+		var a = joint.connectedBody.transform.position - anchor;
+		var b = target - anchor;
+		var x = joint.connectedBody.transform.InverseTransformPoint (Vector3.Cross (a, b)).x;
+		var err = Vector3.Angle (a, b) * Mathf.Sign (x);
+
+		JointSpring s = joint.spring;
+		s.targetPosition -= rate * err;
+		joint.spring = s;
+
+		return target - (b.normalized * joint.connectedBody.transform.localScale.y);
 	}
 
 	void Update () {
-		var anchor = arm1.transform.TransformPoint(joint2.anchor);
-		var a = arm2.transform.position - anchor;
-		var b = ball.transform.position - anchor;
-		// test.transform.position = arm2.transform.position;
-		var x = arm2.transform.InverseTransformPoint (Vector3.Cross (a, b)).x;
-		var e2 = Vector3.Angle (a, b) * Mathf.Sign (x);
-
-		// print (string.Format("{0} {1}", e2, joint2.spring.targetPosition));
-
-		JointSpring hingeSpring = joint2.spring;
-		hingeSpring.targetPosition -= 0.2f * e2;//0.02f * Mathf.Pow(e2, 3);
-		joint2.spring = hingeSpring;
+		Vector3 p = target.transform.position;
+		for (int i = joints.Length - 1; i >= 0; --i) {
+			p = Follow (joints [i], p);
+		}
 	}
 }
---
+++ Assets/ShakyPlatform.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakyPlatform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var noise = Mathf.PerlinNoise (Time.time * 0.1f, 0);
+		gameObject.transform.rotation = Quaternion.Euler (noise*60f-25f, 0, 0);
+	}
+}
