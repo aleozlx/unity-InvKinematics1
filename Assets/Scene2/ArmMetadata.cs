a0507564f57a704409e5f13b0c382a3147fb5a18
Alex Yang <aleozlx@gmail.com>
2017-03-25 16:22:45 -0500

    + scene2

---
+++ Assets/Scene2/ArmMetadata.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmMetadata : MonoBehaviour {
+	public Vector3 ConnectBodyCenter;
+}
---
+++ Assets/Scene2/JointInvKinematics.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointInvKinematics : MonoBehaviour {
+	public HingeJoint[] joints;
+	public GameObject baseObj;
+	public GameObject target;
+
+	void Start () {
+		// target = GameObject.Find ("Ball");
+	}
+
+	static float OddErp(float x, float scale){
+		return Mathf.Sign (x) * (1f - Mathf.Cos (x / scale * Mathf.PI * 0.5f)) * scale;
+	}
+
+//	static Vector3 GetConnectedArmCenter(GameObject robot_arm){
+//		var connectedCenter = robot_arm.GetComponent<ArmMetadata>().ConnectBodyCenter;
+//		return connectedCenter; //robot_arm.transform.TransformPoint(connectedCenter);
+//	}
+
+	Vector3 Follow(HingeJoint joint, Vector3 target){
+		Vector3 a = Vector3.up, b = joint.connectedBody.transform.InverseTransformPoint(target);
+		float err = Vector3.Angle (a, b) * Mathf.Sign (Vector3.Cross(a, b).x);
+//		print (string.Format("a:{0} b:{1}, err:{2}", a, b, err));
+
+		JointSpring s = joint.spring;
+		s.targetPosition -= Mathf.Rad2Deg * OddErp (Mathf.Deg2Rad * 0.75f * err, Mathf.PI);
+		joint.spring = s;
+
+//		return target - (b.normalized * joint.connectedBody.transform.localScale.y);
+		return new Vector3();
+	}
+
+	void Update () {
+		Vector3 p = target.transform.position;
+		for (int i = joints.Length - 1; i > 0; --i) {
+			p = Follow (joints [i], p);
+		}
+	}
+}
