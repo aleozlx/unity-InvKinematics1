cfc7e9c5fc12a6c4bb9c8466041eb8a3ca89cbb0
Alex Yang <aleozlx@gmail.com>
2017-03-24 23:29:37 -0500

    + joint2

--- Assets/InverseKinematics.cs
+++ Assets/InverseKinematics.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InverseKinematics : MonoBehaviour {
-	public GameObject ball;
-	public HingeJoint platformHindge;
+	public GameObject ball, test;
+	public GameObject arm1, arm2;
+	public HingeJoint joint1, joint2;
 
 	void Start () {
-		ball = GameObject.Find("Ball");
-		platformHindge = GetComponent<HingeJoint>();
+		ball = GameObject.Find ("Ball");
+		arm1 = GameObject.Find ("Arm1");
+		arm2 = GameObject.Find ("Arm2");
+		test = GameObject.Find ("Test");
+		joint1 = GetComponent<HingeJoint>();
+		joint2 = arm1.GetComponent<HingeJoint> ();
 	}
 
 	void Update () {
-		JointSpring hingeSpring = platformHindge.spring;
-		hingeSpring.targetPosition = ball.transform.localPosition.y;
-		platformHindge.spring = hingeSpring;
+		var anchor = arm1.transform.TransformPoint(joint2.anchor);
+		var a = arm2.transform.position - anchor;
+		var b = ball.transform.position - anchor;
+		// test.transform.position = arm2.transform.position;
+		var x = arm2.transform.InverseTransformPoint (Vector3.Cross (a, b)).x;
+		var e2 = Vector3.Angle (a, b) * Mathf.Sign (x);
+
+		// print (string.Format("{0} {1}", e2, joint2.spring.targetPosition));
+
+		JointSpring hingeSpring = joint2.spring;
+		hingeSpring.targetPosition -= 0.2f * e2;//0.02f * Mathf.Pow(e2, 3);
+		joint2.spring = hingeSpring;
 	}
 }
